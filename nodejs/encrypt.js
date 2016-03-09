6d7ee612ac1141f0ba2f9fdd537d1b312eef7fc7
Martin Thomson <martin.thomson@gmail.com>
2016-03-09 13:59:47 +1100

    Adding basic encrypt utility

---
+++ nodejs/encrypt.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var base64 = require('urlsafe-base64');
+var crypto = require('crypto');
+var ece = require('./ece.js');
+
+if (process.argv.length < 5) {
+  console.warn('Usage: ' + process.argv.slice(0, 2).join(' ') +
+               ' <key> <salt> <message> [JSON args]');
+  process.exit(2);
+}
+
+var params = {
+  key: process.argv[2],
+  salt: process.argv[3]
+};
+
+if (process.argv.length > 5) {
+  var extra = JSON.parse(process.argv[5]);
+  Object.keys(extra).forEach(function(k) {
+    params[k] = extra[k];
+  });
+}
+
+console.log("Params: " + JSON.stringify(params, null, 2));
+var result = ece.encrypt(base64.decode(process.argv[4]), params);
+
+console.log("Encrypted Message: " + base64.encode(result));
