95c4049ed4a5f0e388b77242bb09eff4f69fcc3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-22 22:27:28 +0100

    Support Firefox 46-47 intermediate standard in Python lib as well

--- python/http_ece/__init__.py
+++ python/http_ece/__init__.py
@@ -67,8 +67,12 @@ def deriveKey(mode, salt, key=None, dh=None, keyid=None, authSecret=None, padSiz
         keyinfo = buildInfo(b"aesgcm", context)
         nonceinfo = buildInfo(b"nonce", context)
     elif padSize == 1:
-        keyinfo = b"Content-Encoding: aesgcm128"
-        nonceinfo = b"Content-Encoding: nonce"
+        if authSecret is not None:
+          keyinfo = buildInfo(b"aesgcm128", context)
+          nonceinfo = buildInfo(b"nonce", context)
+        else:
+          keyinfo = b"Content-Encoding: aesgcm128"
+          nonceinfo = b"Content-Encoding: nonce"
     else:
         raise Exception(u"unable to set context for padSize=" + str(padSize))
 
