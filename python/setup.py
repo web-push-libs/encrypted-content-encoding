9a22cc25572e54ed34162c165ccf81590b487b10
Martin Thomson <martin.thomson@gmail.com>
2016-12-22 16:57:56 +1100

    Update version number for python

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='http_ece',
-    version='0.5.0',
+    version='0.6.0',
     author='Martin Thomson',
     author_email='martin.thomson@gmail.com',
     scripts=[],
