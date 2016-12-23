cfb3cbd061dfd0a6fe6a0cb1b47bbf8ccd14913d
Martin Thomson <martin.thomson@gmail.com>
2016-12-23 16:36:43 +1100

    Bump version number

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ with io.open(os.path.join(here, 'README.rst'), encoding='utf8') as f:
 
 setup(
     name='http_ece',
-    version='0.6.3',
+    version='0.6.4',
     author='Martin Thomson',
     author_email='martin.thomson@gmail.com',
     scripts=[],
